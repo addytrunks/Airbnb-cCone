730d8113d49f1e16522b11a9022f66a0a1dcbf90
Adhithya Srivatsan <addytrunks@gmail.com>
2022-03-24 14:46:06 +0530

    Created LargeCard Component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
+import LargeCard from '../components/LargeCard'
 import MediumCard from '../components/MediumCard'
 import SmallCard from '../components/SmallCard'
 
@@ -39,6 +40,14 @@ export default function Home({exploreData,cardsData}) {
           </div>
 
         </section>
+
+        {/* Large Card */}
+        <LargeCard 
+          image="https://links.papareact.com/4cj"
+          title="The Greatest Outdoors"
+          description="Wishes curated by airbnb"
+          buttonText="Get Inspired"
+        />
       </main>
     </div>
   )
