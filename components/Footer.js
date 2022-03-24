20fdc49cda9f8ef98acb18436caede9db8eedda4
Adhithya Srivatsan <addytrunks@gmail.com>
2022-03-24 15:07:38 +0530

    Implemented Footer

---
+++ components/Footer.js
@@ -0,0 +1,45 @@
+
+const Footer = () => {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-[#191919]">
+        <div className="space-y-4 text-xs text-white">
+            <h5 className="font-bold">ABOUT</h5>
+            <p>How Airbnb works</p>
+            <p>NewsRoom</p>
+            <p>Investors</p>
+            <p>Airbnb Plus</p>
+            <p>Airbnb Luxe</p>
+        </div>
+
+        <div className="space-y-4 text-xs text-white">
+            <h5 className="font-bold">COMMUNITY</h5>
+            <p>Accessibility</p>
+            <p>This is not a real site</p>
+            <p>Its a nice clone</p>
+            <p>Referrals Accepted</p>
+            <p>React JS</p>
+        </div>
+
+        <div className="space-y-4 text-xs text-white">
+            <h5 className="font-bold">HOST</h5>
+            <p>Presents</p>
+            <p>Hundreds of Hosts</p>
+            <p>Book Now</p>
+            <p>Next JS</p>
+            <p>Zero to Full Stack</p>
+        </div>
+
+        <div className="space-y-4 text-xs text-white">
+            <h5 className="font-bold">SUPPORT</h5>
+            <p>Help Centre</p>
+            <p>Trust & Safety</p>
+            <p>Easter Eggs</p>
+            <p>For the Win</p>
+            <p>Say Hi</p>
+        </div>
+
+    </div>
+  )
+}
+
+export default Footer
