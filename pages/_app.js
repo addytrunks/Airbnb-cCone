629f3a80998e2b2576d97c3f75f9636da986c87e
Adhithya Srivatsan <addytrunks@gmail.com>
2022-03-25 22:29:22 +0530

    Implemented Progress bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,18 @@
 import '../styles/globals.css'
+import ProgressBar from '@badrap/bar-of-progress'
+import Router from 'next/router'
+
+const progress = new ProgressBar({
+  size:3,
+  color:'#C8494D',
+  className:'z-50',
+  delay:100,
+})
+
+// The nextjs router fires off certain events when we change routes, we want our progress bar to start at that time.
+Router.events.on('routeChangeStart',progress.start)
+Router.events.on('routeChangeComplete',progress.finish)
+Router.events.on('routeChangeError',progress.finish)
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
